d45476ccf7d0d8be4ff8c12ec97c0a831272cab9
Muslimakhon Shokirova <shokirovamuslimakhon@gmail.com>
2023-12-30 15:57:06 +0500

    switch

--- my.js
+++ my.js
@@ -14,3 +14,21 @@ for(let x= 2; x<5; x++){
 console.log(x);
 }
 
+
+// switch case
+
+let colors = ["red" , "yellow" , "sky blue"]
+
+switch(colors){
+  case "red" :
+    console.log("Borma kuyasan");
+  case "yellow" :
+    console.log("Hoy kuyasan");
+    case "sky blue" :
+      console.log("Ana kuyding"); 
+
+      default:
+        console.log("you still alive, congrats!");
+  }
+
+  
