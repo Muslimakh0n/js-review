36b22719a4debc28f4616f5997467d493a5a81c9
Muslimakhon Shokirova <shokirovamuslimakhon@gmail.com>
2024-01-05 10:56:11 +0500

    solveeeeeed

--- my.js
+++ my.js
@@ -53,14 +53,24 @@ let array = [1, 2, 3, 4, 5];
 console.log(findAverage(array));
 
 
-function removeLastEl(str){
-  if (((str.length)-1)==="!") {
-    return str.slice(0,1)
-  }else{
-    return str
+// function removeLastEl(str){
+//   if (((str.length)-1)==="!") {
+//     return str.slice(0,1)
+//   }else{
+//     return str
+//   }
+// }
+
+// console.log(removeLastEl("Hello!"));
+
+// solve it, it's not working
+
+function removeLastEl(str) {
+  if (str.charAt(str.length - 1) === "!") {
+    return str.slice(0, -1);
+  } else {
+    return str;
   }
 }
 
 console.log(removeLastEl("Hello!"));
-
-// solve it, it's not working
